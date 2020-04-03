5cd540c3ada051a753d2a1592a29b2b7ba968ae9
Googler <knowbeeinc@gmail.com>
2020-04-03 19:34:14 +0200

    Divide Watermelon

--- katas.go
+++ katas.go
@@ -431,3 +431,12 @@ func Solution(word string) string {
 	}
 	return reversed
 }
+
+// Divide https://www.codewars.com/kata/55192f4ecd82ff826900089e/train/go
+// Divide Watermelon
+func Divide(weight int) bool {
+	if weight == 2 {
+		return false
+	}
+	return weight%2 == 0
+}
--- katas_test.go
+++ katas_test.go
@@ -344,4 +344,16 @@ var _ = Describe("Katas", func() {
 	It("should test that the solution returns the correct value", func() {
 		Expect(Solution("world")).To(Equal("dlrow"))
 	})
+	It("should test that the solution returns the correct value", func() {
+		Expect(Divide(4)).To(Equal(true))
+		Expect(Divide(2)).To(Equal(false))
+		Expect(Divide(5)).To(Equal(false))
+		Expect(Divide(88)).To(Equal(true))
+		Expect(Divide(100)).To(Equal(true))
+		Expect(Divide(67)).To(Equal(false))
+		Expect(Divide(90)).To(Equal(true))
+		Expect(Divide(10)).To(Equal(true))
+		Expect(Divide(99)).To(Equal(false))
+		Expect(Divide(32)).To(Equal(true))
+	})
 })
